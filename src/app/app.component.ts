1060f0bf127c7f59cd701c43682a0f2900299d27
Italo Oliveira <italooliveira-dev@hotmail.com>
2023-12-23 13:52:43 -0300

    feat: creating the base structure for the beginning of the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HomeComponent]
+})
+export class AppComponent {
+
+}
