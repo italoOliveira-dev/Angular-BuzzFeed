2173c69e9a10ac40d84ca4d0994714e2135f056d
Italo Oliveira <italooliveira-dev@hotmail.com>
2023-12-24 00:06:42 -0300

    feat: :sparkles: creating logical part of the project

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-quizz',
-  standalone: true,
-  imports: [],
-  templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.css'
+    selector: 'app-quizz',
+    standalone: true,
+    imports: [],
+    templateUrl: './quizz.component.html',
+    styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
+
+    title: string = '';
+
+    questions: any;
+    questionSelected: any;
+
+    answers: string[] = [];
+    answersSelected: string = '';
+
+    questionIndex: number = 0;
+    questionMaxIndex: number = 0;
+
+    finished: boolean = false;
+
 
 }
